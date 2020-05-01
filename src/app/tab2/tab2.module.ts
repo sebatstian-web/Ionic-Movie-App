eaf101350b63ad884feb3479debd116361241c4e
Sebastián <sebtstian@gmail.com>
2020-05-01 17:15:11 -0300

    diseño pagina de busqueda

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -7,6 +7,8 @@ import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { PipesModule } from '../pipes/pipes.module';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
@@ -14,8 +16,10 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab2PageRoutingModule
+    Tab2PageRoutingModule,
+    PipesModule,
+    ComponentsModule,
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page],
 })
 export class Tab2PageModule {}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 import { ApisService } from '../services/apis.service';
+import { Movie } from '../interfaces/interfaces';
+import { DetailComponent } from '../components/detail/detail.component';
 
 @Component({
   selector: 'app-tab2',
@@ -9,11 +12,41 @@ import { ApisService } from '../services/apis.service';
 })
 export class Tab2Page {
   textSearch: string;
-  suggestions: Array<string> = ['SpiderMan', 'Avengers', 'Toy Story'];
+  suggestions: Array<string> = [
+    'Matrix',
+    'Volver al futuro',
+    'Toy Story',
+    'Kong',
+  ];
+  movies: Array<Movie> = [];
+  loading: boolean;
 
-  constructor(private apis: ApisService) {}
+  constructor(private apis: ApisService, private modalCtrl: ModalController) {}
 
   searchMovie(event: CustomEvent) {
-    this.apis.searchMovieService(event.detail.value).subscribe(console.log);
+    const value: string = event.detail.value;
+
+    if (value.length === 0) {
+      this.loading = false;
+      this.movies = [];
+      return;
+    }
+
+    this.loading = true;
+    this.apis.searchMovieService(event.detail.value).subscribe((resp) => {
+      this.movies = resp.results;
+      this.loading = false;
+    });
+  }
+
+  async detailMovie(id: string) {
+    const modal = await this.modalCtrl.create({
+      component: DetailComponent,
+      componentProps: {
+        id,
+      },
+    });
+
+    modal.present();
   }
 }
